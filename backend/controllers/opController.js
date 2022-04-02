686962ecab6346425829d63681510b499151d806
Samuel Ng <ngsamu4@gmail.com>
2022-04-01 20:11:26 -0400

    Post delta + update quill contents.

--- backend/controllers/opController.js
+++ backend/controllers/opController.js
@@ -1,8 +1,12 @@
-// TODO: Async handler with error middleware
+const { clients } = require('../server')
 
 const updateDocument = async (req, res) => {
-  // TODO: implement
-  res.json({})
+  if (!req.body) throw new Error('Missing body.')
+  if (!req.params) throw new Error('No connection id specified.')
+
+  const clientId = req.params.id
+  clients[clientId].doc.submitOp(req.body, { source: clientId })
+  res.sendStatus(200)
 }
 
 module.exports = {
