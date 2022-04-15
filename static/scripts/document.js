14aaabb7bd19a2fe7c63364f6a639d8328de5e5b
Zhen Yi Pan <zharnite@gmail.com>
2022-04-15 02:02:19 -0400

    document.js logs

--- static/scripts/document.js
+++ static/scripts/document.js
@@ -4,23 +4,16 @@ const { v4: uuidv4 } = require('uuid')
 const axios = require('axios')
 const tinycolor = require('tinycolor2')
 
-Quill.register('modules/cursors', QuillCursors)
-
 const ID = uuidv4()
-
 const path = window.location.pathname
 const docID = path.split('/').slice(-1)[0]
-let version = -1
-let waitingForAck = false // flag to identify waiting for server's ack
-let opQueue = []
-
-// Cursor colors
-let colors = {}
 
 // Set up event stream to listen to events from server
 const evtSource = new EventSource(`/doc/connect/${docID}/${ID}`)
 
-// Toolbar options
+// Set up quill
+Quill.register('modules/cursors', QuillCursors)
+let colors = {}
 const toolbar = [
   [{ header: [1, 2, 3, false] }],
   ['bold', 'italic', 'underline', 'strike'],
@@ -29,8 +22,6 @@ const toolbar = [
   ['clean'],
   ['image'],
 ]
-
-// Set up quill
 const quill = new Quill('#editor', {
   theme: 'snow',
   modules: {
@@ -46,7 +37,7 @@ const quill = new Quill('#editor', {
 const cursors = quill.getModule('cursors')
 const Delta = Quill.import('delta')
 
-// Image handler
+// Quill image handler
 function imageHandler() {
   let range = this.quill.getSelection()
   let value = prompt('paste img url')
@@ -55,8 +46,17 @@ function imageHandler() {
   }
 }
 
-// Helper to send post op
+// Variables for submitting ops to server
+let version = -1
+let waitingForAck = false // flag to identify waiting for server's ack
+let opQueue = []
+
+// Post an op to server
 const postOp = () => {
+  // console.log(`attempting to post op from client ${ID} to doc ${docID}`)
+  // console.log(`op to send to server: `, op)
+  // console.log(`version to send to server: ${version}`)
+
   axios
     .post(`/doc/op/${docID}/${ID}`, {
       version,
@@ -64,19 +64,25 @@ const postOp = () => {
     })
     .then((res) => {
       if (res.data.status === 'ok') {
-        // do nothing
+        // console.log('post op succeeded with status ok')
       } else if (res.data.status === 'retry') {
         postOp()
+        // console.log('post op failed with status retry')
       }
     })
 }
 
-// Helper to send op request
-const sendOpQueue = async () => {
-  // console.log('attempting to send opqueue')
+// Attempt to send a queued op to the server
+const sendAnOpFromQueue = async () => {
+  // console.log('attempting to send an op from queue')
+  // console.log(`waitingForAck: ${waitingForAck}`)
+  // console.log(`opQueue.length: ${opQueue.length}`)
   if (!waitingForAck && opQueue.length > 0) {
-    // console.log('submitting: ', JSON.stringify({ version, op: opQueue[0] }))
+    // console.log('conditions met, proceeding')
+    // console.log(`waitingForAck set to true`)
+
     waitingForAck = true
+    postOp()
 
     // Submit op and wait for response
     // let response = await axios.post(`/doc/op/${docID}/${ID}`, {
@@ -93,55 +99,64 @@ const sendOpQueue = async () => {
     //     op: opQueue[0],
     //   })
     // }
-
-    postOp()
+  } else {
+    // console.log('failed to send an op from queue, conditions not met')
   }
-  // console.log('exit sending opqueue')
 }
 
 // Send changes we made to quill
 quill.on('text-change', (delta, oldDelta, source) => {
-  // FIXME: delete
-  // console.log('delta test')
-  // const a = new Delta().insert('a')
-  // const b = new Delta().insert('b').retain(5).insert('c')
-  // const aTrue = a.transform(b, true) // new Delta().retain(1).insert('b').retain(5).insert('c');
-  // const aFalse = a.transform(b, false) // new Delta().insert('b').retain(6).insert('c');
-  // console.log(aTrue)
-  // console.log(aFalse)
-
-  // console.log('opqueue: ', opQueue)
+  // console.log(`quill text-change event`)
+  // console.log(`delta: `, delta)
+  // console.log(`oldDelta: `, oldDelta)
+  // console.log(`source: `, source)
 
   // Don't send changes to shareDB if we didn't make the change
   if (source !== 'user') {
+    // console.log('source is user, changes not pushed to queue')
     return
   }
-  // console.log('Delta ' + JSON.stringify(delta))
-  // console.log('Delta ' + JSON.stringify(delta.ops))
 
-  // Store op in queue
   opQueue.push(delta.ops)
-  sendOpQueue()
+  // console.log(`op pushed to queue`)
+  // console.log(`op pushed: `, delta.ops)
+  // console.log('opQueue: ', opQueue)
+
+  sendAnOpFromQueue()
 })
 
 // Send cursor changes we made on quill
 quill.on('selection-change', (range, oldRange, source) => {
+  // console.log(`quill selection-change event`)
+  // console.log(`range: `, range)
+  // console.log(`oldRange: `, oldRange)
+  // console.log(`source: `, source)
+
   // Don't send changes to shareDB if we didn't make the change
   if (source !== 'user') {
+    // console.log('source is user, selection change not sent to server')
     return
   }
 
   // If range is null, indicates focus lost => appears to everyone else that cursor is where it was before
-  if (!range) return
+  if (!range) {
+    // console.log(`range is null, indicates focus lost`)
+    return
+  }
 
-  axios.post(`/doc/presence/${docID}/${ID}`, range)
+  // console.log(`posting presence from client ${ID} to doc ${docID}`)
+  axios.post(`/doc/presence/${docID}/${ID}`, range).then((res) => {
+    // console.log(`presence update callback with res.status: `, res.status)
+  })
 })
 
 // Update quill when message is received from server event stream
 evtSource.onmessage = (event) => {
   const data = JSON.parse(event.data)
-  // console.log('data from evtSource: ', data)
+  // console.log(`received message from event source: `, data)
   if (data.presence) {
+    // Presence data
+    // console.log(`presence: `, data)
     const { id, cursor } = data.presence
     colors[id] = colors[id] || tinycolor.random().toHexString()
     if (cursor) {
@@ -150,29 +165,29 @@ evtSource.onmessage = (event) => {
     } else {
       cursors.removeCursor(id)
     }
-  } else if (data.ack) {
-    // Acknowledged our change
-    // console.log('acked: ', data)
-    version += 1
-    waitingForAck = false
-    opQueue.shift() // remove from queue after we have acknowledged
-
-    sendOpQueue()
   } else if (data.content) {
     // Get inital document
     // console.log('initial doc: ', data)
     quill.setContents(data.content)
     version = data.version
+  } else if (data.ack) {
+    // Acknowledged our change
+    // console.log('acked op: ', data)
+    version += 1
+    waitingForAck = false
+    opQueue.shift() // remove from queue after we have acknowledged
+
+    sendAnOpFromQueue()
   } else {
-    // Update doc contents from other clients
-    // data.forEach((oplist) => quill.updateContents(oplist))
-    // console.log('update doc from other clients: ', data)
+    // Update our doc contents based on ops from other clients
+    // console.log('op from other clients: ', data)
     version += 1
 
-    let incomingOp = new Delta(data)
-
-    let updatedIncomingOp = new Delta(data)
     // Apply transformations
+    let incomingOp = new Delta(data)
+    let updatedIncomingOp = new Delta(data)
+    // console.log(`opQueue before tranforms: `, opQueue)
+    // console.log(`incomingOp before tranforms: `, incomingOp)
     opQueue = opQueue.map((queueOp) => {
       queueOp = new Delta(queueOp)
 
@@ -181,6 +196,8 @@ evtSource.onmessage = (event) => {
 
       return newQueueOp
     })
+    // console.log(`opQueue after tranforms: `, opQueue)
+    // console.log(`incomingOp after tranforms: `, updatedIncomingOp)
 
     quill.updateContents(updatedIncomingOp)
   }
