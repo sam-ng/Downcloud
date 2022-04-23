b45365b2caf3467906c915f371e24907d6c161be
Zhen Yi Pan <zharnite@gmail.com>
2022-04-23 12:50:18 -0400

    function name typo

--- backend/controllers/indexController.js
+++ backend/controllers/indexController.js
@@ -12,7 +12,7 @@ const getSearchResults = asyncHandler(async (req, res) => {
 // @desc    Get suggestion
 // @route   GET /index/suggest
 // @access  Private
-const getSearchResults = asyncHandler(async (req, res) => {
+const getSuggestion = asyncHandler(async (req, res) => {
   // TODO:
   res.set('X-CSE356', '61f9c5ceca96e9505dd3f8b4').json({})
 })
